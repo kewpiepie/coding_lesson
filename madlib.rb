769a7fa01c111c4105185fc9e5fe6d4a88e80fec
Melanie Lund <melanielund@Melanies-MacBook-Air.local>
2018-05-24 01:35:14 -0700

    add madlib

---
+++ madlib.rb
@@ -0,0 +1,19 @@
+ #I am trying to make a mad lib
+# this is a code comment 
+
+puts "Enter a color: "
+color = gets.chomp ()
+puts "Enter a plural noun: "
+plural_noun = gets.chomp ()
+puts "Enter a celebrity: "
+celebrity = gets.chomp ()
+
+
+
+#puts "Roses are red"
+#puts "Violets are blue"
+#puts "I love you"
+
+puts "Roses are " + color
+puts plural_noun + "are blue " 
+puts "I love "  + celebrity 
